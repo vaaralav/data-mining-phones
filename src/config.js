f3e45c4e4c9881dd46360ce02d56fc4da9ecb4ee
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-05-23 13:18:06 +0700

    Revisit phones to search

--- src/config.js
+++ src/config.js
@@ -9,6 +9,7 @@ export const PhonesToSearch: Array<PhoneSearch> = [
   {device: 'iphone 6s', brand: 'apple'},
   {device: 'iphone 7 plus', brand: 'apple'},
   {device: '3t', brand: 'oneplus'},
+  {device: '3', brand: 'oneplus'},
   {device: 'lumia 950', brand: 'microsoft'},
   {device: 'elite x3', brand: 'hp'},
   {device: 'r9s', brand: 'oppo'},
